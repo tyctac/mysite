d05f9c48adc33c1281de43ee8aeef2f7443c17c4
tyctac <2283499971@qq.com>
2016-08-11 22:21:54 +0800

    before amend

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.db import models
 from django.template import loader
-from .models import Question
+from .models import Question,Choice
 from django.core.urlresolvers import reverse
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('polls/index.html')
 	context = {
-		'latest_question_list':latest_question_list,
+		'latest_question_list': latest_question_list,
 	}
-	#output = ','.join([q.question_text for q in latest_question_list])
-	return HttpResponse(template.render(context,request))
+	return HttpResponse(template.render(context, request))	
+
 def home(request):
-	return render(request,'index.html')
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)
+
 
 def detail(request, question_id):
 	question = get_object_or_404(Question,pk=question_id)
